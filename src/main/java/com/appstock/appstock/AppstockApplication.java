3c29c1a417517bd72311b64b4792c8c5b00da893
angelsimoonn <angelsimoonn@gmail.com>
2025-11-06 12:53:06 +0100

    Initial commit

---
+++ src/main/java/com/appstock/appstock/AppstockApplication.java
@@ -0,0 +1,13 @@
+package com.appstock.appstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appstock/appstock/AppstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appstock.appstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
